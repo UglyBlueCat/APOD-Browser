5e8bec5b894247064a8b622ca1c96b78946daad0
Robin Spinks <uglybluecat@googlemail.com>
2017-04-17 11:44:38 +0100

    Create NetworkManager class to pass test

---
+++ APOD Browser/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  APOD Browser
+//
+//  Created by Robin Spinks on 17/04/2017.
+//  Copyright © 2017 Robin Spinks. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
