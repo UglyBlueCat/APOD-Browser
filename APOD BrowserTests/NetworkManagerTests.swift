f3abda388ed7dad0625f0a2210404cbecc74f8c4
Robin Spinks <uglybluecat@googlemail.com>
2017-04-17 18:10:51 +0100

    test makeRequest returns data

--- APOD BrowserTests/NetworkManagerTests.swift
+++ APOD BrowserTests/NetworkManagerTests.swift
@@ -30,12 +30,27 @@ class NetworkManagerTests: XCTestCase {
     }
     
     func testRequestHandling() {
+        let expect = expectation(description: "waitForNetworkManager")
         
+        var testData : Data = Data()
         let method : String = "GET"
         let urlStr : String = "https://api.nasa.gov/planetary/apod"
         let params : Dictionary<String, String> = ["api_key": "DEMO_KEY"]
         
         networkManager.makeRequest(method: method, params: params, urlStr: urlStr) { (data, urlResponse, error) in
+            guard error == nil else {
+                XCTFail("error: \(String(describing: error))")
+                return
+            }
+            guard data != nil else {
+                XCTFail()
+                return
+            }
+            testData = data!
+            expect.fulfill()
         }
+        
+        waitForExpectations(timeout: 10, handler: nil)
+        XCTAssert(testData.count > 0)
     }
 }
