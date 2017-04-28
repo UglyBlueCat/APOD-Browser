bb1cd77f6a4dfef6503a042dd97d40c957c581c9
Robin Spinks <uglybluecat@googlemail.com>
2017-04-28 18:23:58 +0100

    complate data model

--- APOD Browser/APOD.swift
+++ APOD Browser/APOD.swift
@@ -9,5 +9,33 @@
 import Foundation
 
 class APOD {
+    var copyright : String = ""
+    var date : String = ""
+    var explanation : String = ""
+    var hdurl : String = ""
+    var media_type : String = ""
+    var service_version : String = ""
+    var title : String = ""
+    var url : String = ""
     
+    init() {}
+    
+    init(copyright: String,
+         date: String,
+         explanation: String,
+         hdurl: String,
+         media_type: String,
+         service_version: String,
+         title: String,
+         url: String) {
+        
+        self.copyright = copyright
+        self.date = date
+        self.explanation = explanation
+        self.hdurl = hdurl
+        self.media_type = media_type
+        self.service_version = service_version
+        self.title = title
+        self.url = url
+    }
 }
--- APOD BrowserTests/APODTests.swift
+++ APOD BrowserTests/APODTests.swift
@@ -20,4 +20,46 @@ class APODTests: XCTestCase {
         
         XCTAssertNotNil(apod)
     }
+    
+    func testParameters() {
+        let apod : APOD = APOD()
+        
+        XCTAssertNotNil(apod.copyright)
+        XCTAssertNotNil(apod.date)
+        XCTAssertNotNil(apod.explanation)
+        XCTAssertNotNil(apod.hdurl)
+        XCTAssertNotNil(apod.media_type)
+        XCTAssertNotNil(apod.service_version)
+        XCTAssertNotNil(apod.title)
+        XCTAssertNotNil(apod.url)
+    }
+    
+    func testParamInit() {
+        let copyright = "Subaru, NAOJ"
+        let date = "2017-04-28"
+        let explanation = "Some 60 million light-years away in the southerly constellation Corvus, two large galaxies are colliding. Stars in the two galaxies, cataloged as NGC 4038 and NGC 4039, very rarely collide in the course of the ponderous cataclysm that lasts for hundreds of millions of years. But the galaxies' large clouds of molecular gas and dust often do, triggering furious episodes of star formation near the center of the cosmic wreckage. Spanning over 500 thousand light-years, this stunning view also reveals new star clusters and matter flung far from the scene of the accident by gravitational tidal forces. The remarkable mosaicked image was constructed using data from the ground-based Subaru telescope to bring out large-scale and faint tidal streams, and Hubble Space Telescope data of extreme detail in the bright cores. The suggestive visual appearance of the extended arcing structures gives the galaxy pair its popular name - The Antennae."
+        let hdurl = "https://apod.nasa.gov/apod/image/1704/AntennaeColombari1824x0_q100_watermark.jpg"
+        let media_type = "image"
+        let service_version = "v1"
+        let title = "Exploring the Antennae"
+        let url = "https://apod.nasa.gov/apod/image/1704/AntennaeColombari1024_q100_watermark.jpg"
+        
+        let apod : APOD = APOD(copyright: copyright,
+                               date: date,
+                               explanation: explanation,
+                               hdurl: hdurl,
+                               media_type: media_type,
+                               service_version: service_version,
+                               title: title,
+                               url: url)
+        
+        XCTAssert(apod.copyright == copyright)
+        XCTAssert(apod.date == date)
+        XCTAssert(apod.explanation == explanation)
+        XCTAssert(apod.hdurl == hdurl)
+        XCTAssert(apod.media_type == media_type)
+        XCTAssert(apod.service_version == service_version)
+        XCTAssert(apod.title == title)
+        XCTAssert(apod.url == url)
+    }
 }
