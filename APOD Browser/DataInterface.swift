2db3e1106e4a0ca55af9ba4f70ea2c427d352da4
Robin Spinks <uglybluecat@googlemail.com>
2017-06-07 17:29:10 +0100

    Base DataInterface

---
+++ APOD Browser/DataInterface.swift
@@ -0,0 +1,13 @@
+//
+//  DataInterface.swift
+//  APOD Browser
+//
+//  Created by Robin Spinks on 07/06/2017.
+//  Copyright © 2017 Robin Spinks. All rights reserved.
+//
+
+import Foundation
+
+class DataInterface {
+    
+}
---
+++ APOD BrowserTests/DataInterfaceTests.swift
@@ -0,0 +1,18 @@
+//
+//  DataInterfaceTests.swift
+//  APOD Browser
+//
+//  Created by Robin Spinks on 07/06/2017.
+//  Copyright © 2017 Robin Spinks. All rights reserved.
+//
+
+import XCTest
+@testable import APOD_Browser
+
+class DataInterfaceTests: XCTestCase {
+    
+    let dataInterface = DataInterface()
+    
+    func testAPODObjectSaveAndLoad() {
+    }
+}
