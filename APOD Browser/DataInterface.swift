bc8d2e3305be34170b49e61579ccafc35ad3614f
Robin Spinks <uglybluecat@googlemail.com>
2017-06-08 16:50:29 +0100

    DataInterface

--- APOD Browser/DataInterface.swift
+++ APOD Browser/DataInterface.swift
@@ -10,4 +10,42 @@ import Foundation
 
 class DataInterface {
     
+    enum DataInterfaceError: Error {
+        case parameterReadError(_ : String)
+    }
+    
+    var fileHandler = FileHandler()
+    
+    func saveAPODObject(_ APODDict: Dictionary<String, String>) throws {
+        
+        var APODData = Data()
+        
+        guard let date = APODDict["date"] else {
+            throw DataInterfaceError.parameterReadError("Cannot read date from Dictionary parameter as String")
+        }
+        
+        do {
+            fileHandler = try FileHandler(fileName: date)
+            APODData = try JSONSerialization.data(withJSONObject: APODDict)
+            try fileHandler.write(APODData)
+        } catch {
+            throw error
+        }
+    }
+    
+    func loadAPODObject(_ date : String) throws -> Dictionary<String, String> {
+        
+        var APODData = Data()
+        var resultDict : Dictionary<String, String> = [:]
+        
+        do {
+            fileHandler = try FileHandler(fileName: date)
+            APODData = try fileHandler.read()
+            resultDict = try JSONSerialization.jsonObject(with: APODData) as! Dictionary<String, String>
+        } catch {
+            throw error
+        }
+        
+        return resultDict
+    }
 }
--- APOD BrowserTests/DataInterfaceTests.swift
+++ APOD BrowserTests/DataInterfaceTests.swift
@@ -14,5 +14,38 @@ class DataInterfaceTests: XCTestCase {
     let dataInterface = DataInterface()
     
     func testAPODObjectSaveAndLoad() {
+        
+        let testDict : Dictionary<String, String> = [
+            "copyright" : "Public Domain",
+            "date": "2017-05-01",
+            "explanation": "The bright source near the center is a neutron star, the incredibly dense, collapsed remains of a massive stellar core. Surrounding it is supernova remnant Cassiopeia A (Cas A), a comfortable 11,000 light-years away. Light from the Cas A supernova, the death explosion of a massive star, first reached Earth about 350 years ago.  The expanding debris cloud spans about 15 light-years in this composite X-ray/optical image.  Still hot enough to emit X-rays, Cas A's neutron star is cooling. In fact, years of observations with the orbiting Chandra X-ray Observatory find that the neutron star is cooling rapidly --  so rapidly that researchers suspect a large part of the neutron star's core is forming a frictionless neutron superfluid. The Chandra results represent the first observational evidence for this bizarre state of neutron matter.",
+            "hdurl": "https://apod.nasa.gov/apod/image/1705/casa_main_960.jpg",
+            "media_type": "image",
+            "service_version": "v1",
+            "title": "Cooling Neutron Star",
+            "url": "https://apod.nasa.gov/apod/image/1705/casa_main_960.jpg"]
+        var resultDict : Dictionary<String, String> = [:]
+        
+        do {
+            try dataInterface.saveAPODObject(testDict)
+        } catch {
+            XCTFail("Error saving object: \(error.localizedDescription)")
+        }
+        
+        do {
+            resultDict = try dataInterface.loadAPODObject(testDict["date"]!)
+        } catch {
+            XCTFail("Error loading object: \(error.localizedDescription)")
+        }
+        
+        XCTAssertEqual(testDict.count, resultDict.count)
+        XCTAssertEqual(testDict["copyright"], resultDict["copyright"])
+        XCTAssertEqual(testDict["date"], resultDict["date"])
+        XCTAssertEqual(testDict["explanation"], resultDict["explanation"])
+        XCTAssertEqual(testDict["hdurl"], resultDict["hdurl"])
+        XCTAssertEqual(testDict["media_type"], resultDict["media_type"])
+        XCTAssertEqual(testDict["service_version"], resultDict["service_version"])
+        XCTAssertEqual(testDict["title"], resultDict["title"])
+        XCTAssertEqual(testDict["url"], resultDict["url"])
     }
 }
