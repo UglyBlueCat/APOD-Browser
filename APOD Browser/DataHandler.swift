a850777632c90fd87dc7e0d2460a5cf724c68a0d
Robin Spinks <uglybluecat@googlemail.com>
2017-06-09 17:35:19 +0100

    DataHandler handles APOD object data

--- APOD Browser/DataHandler.swift
+++ APOD Browser/DataHandler.swift
@@ -10,7 +10,40 @@ import Foundation
 
 class DataHandler {
     
-    func newData(data: Data) {
+    /**
+     Converts a raw data object to JSON, which is then passed to populateResults
+     
+     - parameter newData: The raw data
+     */
+    func newData (_ newData: Data) {
+        do {
+            let jsonData = try JSONSerialization.jsonObject(with: newData, options: .mutableLeaves)
+            self.populateResults(jsonData: jsonData as AnyObject)
+        } catch {
+            DLog("JSON conversion error: \(error)")
+        }
+    }
+    
+    /**
+     Separates a JSON Array into JSON Dictionary objects.
+     
+     - parameter jsonData: The JSON Array
+     */
+    func populateResults (jsonData: AnyObject) {
+        if let APODObject = jsonData as? Dictionary<String, String> {
+            handleAPODObject(APODObject)
+        } else {
+            DLog("Cannot convert data")
+        }
+    }
+    
+    func handleAPODObject(_ APODObject : Dictionary<String, String>) {
         
+        let dataInterface = DataInterface()
+        do {
+            try dataInterface.saveAPODObject(APODObject)
+        } catch {
+            DLog("Error saving data: \(error.localizedDescription)")
+        }
     }
 }
--- APOD BrowserTests/DataHandlerTests.swift
+++ APOD BrowserTests/DataHandlerTests.swift
@@ -12,14 +12,62 @@ import XCTest
 class DataHandlerTests: XCTestCase {
     
     let dataHandler : DataHandler = DataHandler()
+    let testDict : Dictionary<String, String> = [
+        "copyright" : "Public Domain",
+        "date": "2017-05-01",
+        "explanation": "The bright source near the center is a neutron star, the incredibly dense, collapsed remains of a massive stellar core. Surrounding it is supernova remnant Cassiopeia A (Cas A), a comfortable 11,000 light-years away. Light from the Cas A supernova, the death explosion of a massive star, first reached Earth about 350 years ago.  The expanding debris cloud spans about 15 light-years in this composite X-ray/optical image.  Still hot enough to emit X-rays, Cas A's neutron star is cooling. In fact, years of observations with the orbiting Chandra X-ray Observatory find that the neutron star is cooling rapidly --  so rapidly that researchers suspect a large part of the neutron star's core is forming a frictionless neutron superfluid. The Chandra results represent the first observational evidence for this bizarre state of neutron matter.",
+        "hdurl": "https://apod.nasa.gov/apod/image/1705/casa_main_960.jpg",
+        "media_type": "image",
+        "service_version": "v1",
+        "title": "Cooling Neutron Star",
+        "url": "https://apod.nasa.gov/apod/image/1705/casa_main_960.jpg"]
     
-    func testDataHandlerExists() {
-        XCTAssertNotNil(dataHandler)
+    override func tearDown() {
+        super.tearDown()
+        
+        var fileHandler = FileHandler()
+        
+        guard let date = testDict["date"] else {
+            XCTFail("Cannot read date from Dictionary parameter as String")
+            return
+        }
+        
+        do {
+            fileHandler = try FileHandler(fileName: date)
+            try fileHandler.delete()
+        } catch {
+            XCTFail("Error deleting file: \(error.localizedDescription)")
+        }
     }
     
-    func testDataHandlerCanReceiveData() {
-        let data : Data = Data()
+    func testDataHandlerStoresData() {
         
-        dataHandler.newData(data: data)
+        var testData = Data()
+        var resultDict : Dictionary<String, String> = [:]
+        let dataInterface = DataInterface()
+        
+        do {
+            testData = try JSONSerialization.data(withJSONObject: testDict)
+        } catch {
+            XCTFail("Error converting testDict to Data: \(error.localizedDescription)")
+        }
+        
+        dataHandler.newData(testData)
+        
+        do {
+            resultDict = try dataInterface.loadAPODObject(testDict["date"]!)
+        } catch {
+            XCTFail("Error loading object: \(error.localizedDescription)")
+        }
+        
+        XCTAssertEqual(testDict.count, resultDict.count)
+        XCTAssertEqual(testDict["copyright"], resultDict["copyright"])
+        XCTAssertEqual(testDict["date"], resultDict["date"])
+        XCTAssertEqual(testDict["explanation"], resultDict["explanation"])
+        XCTAssertEqual(testDict["hdurl"], resultDict["hdurl"])
+        XCTAssertEqual(testDict["media_type"], resultDict["media_type"])
+        XCTAssertEqual(testDict["service_version"], resultDict["service_version"])
+        XCTAssertEqual(testDict["title"], resultDict["title"])
+        XCTAssertEqual(testDict["url"], resultDict["url"])
     }
 }
